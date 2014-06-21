16e821c023bd525099cab3bfee779a5445f52423
Kevin Grandon <kevingrandon@yahoo.com>
2014-06-21 10:38:38 -0700

    Iterate elements in order

--- export.js
+++ export.js
@@ -1,7 +1,31 @@
 var fs = require('fs');
 
-if (!fs.existsSync(__dirname + '/config.json')) {
+var configPath = __dirname + '/config.json';
+var elementsPath = __dirname + '/elements.json';
+
+if (!fs.existsSync(configPath)) {
 	console.log('Please create a config.json file before beginning. See config.json.tpl for an example.');
 	return;
 }
 
+var config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+var elements = JSON.parse(fs.readFileSync(elementsPath, 'utf-8'));
+var remainingElements = Object.keys(elements);
+
+console.log('Exporting elements from ' + config.GAIA_PATH);
+
+exportNextElement();
+
+function exportNextElement() {
+	var elementKey = remainingElements.shift();
+	if (!elementKey) {
+		return done();
+	}
+
+	console.log('Exporting: ' + elementKey);
+	exportNextElement();
+}
+
+function done() {
+	console.log('All done.');
+}
